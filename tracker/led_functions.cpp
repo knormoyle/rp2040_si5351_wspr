9f0162cb5e9195e8437c20764016e1a89e629f74
Kevin Normoyle <knormoyle@gmail.com>
2024-11-12 20:44:52 -0700

    linting

--- tracker/led_functions.cpp
+++ tracker/led_functions.cpp
@@ -1,39 +1,26 @@
 // Project: https://github.com/knormoyle/rp2040_si5351_wspr
 // Distributed with MIT License: http://www.opensource.org/licenses/mit-license.php
-// Author: Kevin Normoyle AD6Z initial 11/2024
-
-// Arduino IDE main: https://github.com/knormoyle/rp2040_si5351_wspr/tree/main/tracker
-// Arduino IDE libraries: https://github.com/knormoyle/rp2040_si5351_wspr/tree/main/libraries
-
-// Incorporates work by: Kazuhisa “Kazu” Terasaki AG6NS. Thank you.
-// https://github.com/kaduhi/sf-hab_rp2040_picoballoon_tracker_pcb_gen1
-// https://github.com/kaduhi/LightAPRS-W-2.0/tree/port_to_ag6ns_rp2040_picoballoon_tracker
-
-// Incorporates work by: Rob Votin KC3LBR. Thank you.
-// https://github.com/EngineerGuy314/pico-WSPRer
-
+// Author/Gather: Kevin Normoyle AD6Z initially 11/2024
+// See acknowledgements.txt for the lengthy list of contributions/dependencies.
 #include <Arduino.h>
 #include "led_functions.h"
 
-const int LED_BLINK_ON_PERIOD_USEC=50000;
-const int LED_BLINK_OFF_PERIOD_USEC=300000;
-const int LED_BLINK_PAUSE_PERIOD_USEC=1000000;
+const int LED_BLINK_ON_PERIOD_USEC = 50000;
+const int LED_BLINK_OFF_PERIOD_USEC = 300000;
+const int LED_BLINK_PAUSE_PERIOD_USEC = 1000000;
 
-int statusLEDBlinkCnt=0;
+int statusLEDBlinkCnt = 0;
 
-void initStatusLED(void)
-{
+void initStatusLED(void) {
   pinMode(STATUS_LED_PIN, OUTPUT);
   turnOnLED(true);
 }
 
-void setStatusLEDBlinkCount(int cnt)
-{
+void setStatusLEDBlinkCount(int cnt) {
   statusLEDBlinkCnt = cnt;
 }
 
-void updateStatusLED(void)
-{
+void updateStatusLED(void) {
   static uint32_t nextFlipUsec = 0;
   static int targetBlinkCnt = 0;
   static int currBlinkCnt = 0;
@@ -50,39 +37,34 @@ void updateStatusLED(void)
         turnOnLED(true);
       }
       nextFlipUsec = usec + LED_BLINK_ON_PERIOD_USEC;
-    }
-    else {
+    } else {
       // ON to OFF
       turnOnLED(false);
       if (currBlinkCnt >= targetBlinkCnt) {
         nextFlipUsec = usec + LED_BLINK_PAUSE_PERIOD_USEC;
         targetBlinkCnt = 0;
-      }
-      else {
+      } else {
         nextFlipUsec = usec + LED_BLINK_OFF_PERIOD_USEC;
       }
     }
   }
 }
 
-void turnOnLED(bool turn_on)  
-{
+void turnOnLED(bool turn_on) {
     digitalWrite(STATUS_LED_PIN, (turn_on) ? HIGH : LOW);
 }
 
-bool isLEDOn(void) 
-{
+bool isLEDOn(void) {
     return (digitalRead(STATUS_LED_PIN) ? true : false);
 }
 
-void flipLED(void) 
-{
+void flipLED(void) {
     turnOnLED(!isLEDOn());
 }
 
 // other non-blocking examples to look at
 // parallel led blinking code
-// Output library supports HIGH, LOW, TOGGLE, PULSE, BLINK_WITHOUT_DELAY. 
+// Output library supports HIGH, LOW, TOGGLE, PULSE, BLINK_WITHOUT_DELAY.
 // https://github.com/ArduinoGetStarted/output
 // https://github.com/ArduinoGetStarted/output/tree/master/examples
 
