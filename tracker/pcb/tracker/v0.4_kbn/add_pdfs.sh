d13a7e0d7024ccf12a8ae9024d245856a6518aaf
Kevin Normoyle <knormoyle@gmail.com>
2025-02-11 20:37:42 -0700

    add files for pcb

---
+++ tracker/pcb/tracker/v0.4_kbn/add_pdfs.sh
@@ -0,0 +1,6 @@
+git add ms5351m.pdf
+git add lcsc_datasheet_2404181014_Hangzhou-Ruimeng-Tech-MS5351M_C1509083.pdf
+git add ruimeng_ms5351m_apr22_xonlink.pdf
+git add ruimeng_ms5351m_apr22_xonlink_english.pdf
+git add 2404181014_Hangzhou-Ruimeng-Tech-MS5351M_C1509083.pdf
+git add 2404181014_Hangzhou-Ruimeng-Tech-MS5351M_C1509083_english.pdf
