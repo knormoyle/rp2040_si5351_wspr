acc92c6b50ed75eaaa8aca091a8a02b74e57d51c
Kevin Normoyle <knormoyle@gmail.com>
2025-02-17 17:49:39 -0700

    current

--- tracker/tracker.ino
+++ tracker/tracker.ino
@@ -2308,7 +2308,8 @@ void syncAndSendWspr(uint32_t hf_freq, int txNum, uint8_t *hf_tx_buffer,
     static int EXTRA_DELAY_AFTER_ZERO_SEC;
     // 900 gave 0.2 2/16/25
     // was 700 2/17/25
-    if (USE_SIM65M) EXTRA_DELAY_AFTER_ZERO_SEC = 700;  // milliseconds
+    // 700 was a little late sometimes?
+    if (USE_SIM65M) EXTRA_DELAY_AFTER_ZERO_SEC = 650;  // milliseconds
     // 800 gave 0.2 2/16/25
     // 750 gave 0.1 2/16/25
     // was 700 2/17/25. a little early sometimes
